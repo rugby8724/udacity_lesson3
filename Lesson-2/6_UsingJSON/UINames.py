18eb9c169e56df58b3455e5738812a309947f177
Jeremiah Wise <jeremiah.d.wise@gmail.com>
2017-10-07 14:02:40 -0500

    updated json file

--- Lesson-2/6_UsingJSON/UINames.py
+++ Lesson-2/6_UsingJSON/UINames.py
@@ -1,12 +1,5 @@
-#!/usr/bin/env python3
-#
+
 # Client for the UINames.com service.
-#
-# 1. Decode the JSON data returned by the UINames.com API.
-# 2. Print the fields in the specified format.
-#
-# Example output:
-# My name is Tyler Hudson and the PIN on my card is 4840.
 
 import requests
 
@@ -14,10 +7,12 @@ import requests
 def SampleRecord():
     r = requests.get("http://uinames.com/api?ext&region=United%20States",
                      timeout=2.0)
-    # 1. Add a line of code here to decode JSON from the response.
+    j = r.json()
 
     return "My name is {} {} and the PIN on my card is {}.".format(
-        # 2. Add the correct fields from the JSON data structure.
+        j["name"],
+        j["surname"],
+        j["credit_card"]["pin"]
     )
 
 if __name__ == '__main__':
